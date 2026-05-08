caee6724338f7c8b86b30accef4d995716ca7175
Habiba Nasser <habnasgomaa@gmail.com>
2026-05-09 00:36:55 +0300

    test

--- lib/core/networks/dio_client.dart
+++ lib/core/networks/dio_client.dart
@@ -3,7 +3,6 @@ import '../constants/api_constants.dart';
 import '../utils/local_storage.dart';
 
 class ApiClient {
-  // جعل الـ Dio متغير خاص
   static final Dio _dio = Dio(
     BaseOptions(
       baseUrl: ApiConstants.baseUrl,
@@ -13,8 +12,6 @@ class ApiClient {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
       },
-      // السماح بقراءة الردود حتى لو كانت حالات خطأ (مثل 400 أو 401)
-      // لكي نتمكن من عرض رسالة الخطأ القادمة من السيرفر
       validateStatus: (status) => status != null && status < 500,
     ),
   )..interceptors.addAll([
@@ -29,21 +26,18 @@ class _AuthInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     final String? token = LocalStorage.getToken();
-
-    // تأكدي أن الـ Endpoints الخاصة بالـ Auth لا تأخذ توكن قديم أو فارغ
     if (token != null && token.isNotEmpty) {
       options.headers['Authorization'] = 'Bearer $token';
     }
 
-    return handler.next(options); // استخدام handler.next أفضل للمتابعة
+    return handler.next(options);
   }
 }
 
 class _LoggingInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    print('🚀 REQUEST [${options.method}] => PATH: ${options.path}');
-    // طباعة البيانات المرسلة للتأكد من أنها ليست null
+    print(' REQUEST [${options.method}] => PATH: ${options.path}');
     print('Body Data: ${options.data}');
     return handler.next(options);
   }
@@ -51,8 +45,7 @@ class _LoggingInterceptor extends Interceptor {
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
     print(
-        '✅ RESPONSE [${response.statusCode}] => PATH: ${response.requestOptions.path}');
-    // طباعة الرد القادم للتأكد من وصول البيانات
+        ' RESPONSE [${response.statusCode}] => PATH: ${response.requestOptions.path}');
     print('Response Data: ${response.data}');
     return handler.next(response);
   }
@@ -60,7 +53,7 @@ class _LoggingInterceptor extends Interceptor {
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
     print(
-        '❌ ERROR [${err.response?.statusCode}] => PATH: ${err.requestOptions.path}');
+        ' ERROR [${err.response?.statusCode}] => PATH: ${err.requestOptions.path}');
     print('ERROR MESSAGE: ${err.message}');
     print('ERROR DATA: ${err.response?.data}');
     return handler.next(err);
